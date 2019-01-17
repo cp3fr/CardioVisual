63d3b127ac4dd57585860962c3400cbac0704f62
cp3fr <pfeifferchr@gmail.com>
2019-01-17 14:04:34 +0100

    asynch buffer and logging added

--- experiments/CardioVisualExp/script/initImage.py
+++ experiments/CardioVisualExp/script/initImage.py
@@ -1,4 +1,117 @@
+#import libraries
+import csv
+from os import path
 from time import time
+
+# ======== Start of Replay Buffer class ==================================================
+
+class ReplayBuffer(object):
+
+    #initialization of variables
+    def __init__(self):
+
+        #list of last two onset timestamps
+        self.beats = []
+        
+        #list of interbeat intervals for reading
+        self.ibiRead = [] #default list
+        for i in range(100):
+            self.ibiRead.append(0.8)
+        
+        #list of interbeat intervals for recording
+        self.ibiRecord = []
+        
+        #pointer to the recording list
+        self.idx = 0
+        
+        #time of the next scheduled beat
+        self.tNextBeat = -1
+        
+        self.percentage = 1.0
+        
+        
+    #prepare for a new experimental run    
+    def newRun(self,t,percentage):
+        
+        #empty the beat timestamp list
+        self.beats = []
+        
+        #if the recording list is not empty
+        if len(self.ibiRecord)>0:
+            
+            #copy the recording list to the reading list
+            self.ibiRead = self.ibiRecord
+
+        #empty the recording
+        self.ibiRecord = []
+   
+        #reset the recording index
+        self.idx = 0
+        
+        if percentage > 0: 
+            self.percentage = 100.0 / percentage
+        else:
+            self.percentage = 1
+            
+        
+        #schedule the onset of the next beat
+        self.scheduleNextBeat(t)
+        
+        
+        
+    #PLAY: method for scheduling onset of next beat
+    def scheduleNextBeat(self,t):
+    
+        #next beat is current time plus ibi
+        self.tNextBeat = t + ( self.ibiRead[self.idx] * self.percentage )
+        
+        #raise the ibi index
+        self.idx += 1
+        
+        #if end is reached start again from the beginning
+        if self.idx + 1 > len(self.ibiRead):
+        
+            self.idx = 0
+            
+        
+    #RECORD: add a heartbeat timestamp 
+    def addBeat(self,t):
+            
+        #add beat timestamp to the end of the list        
+        self.beats.append(t)
+        
+        #if at least two values in the list
+        if len(self.beats) >= 2:
+            
+            #if more than two values in the list
+            while len(self.beats) != 2:
+                
+                #successively remove first entries from the list until only 2 are left
+                self.beats.pop(0)
+           
+            #then calculate the difference between the remaining beat timestamps
+            val = self.beats[1] - self.beats[0]
+            
+            #check if value within reasonable limits
+            if val > 0.2 and val < 2.0:
+            
+                #add the value to the record list
+                self.ibiRecord.append(val)
+            
+    
+    #read the next interbeat interval
+    def getBeat(self,t):
+        
+        if (self.tNextBeat > 0 ) and ( t - self.tNextBeat >= 0 ) and ( t - self.tNextBeat <= 0.300 ):
+            
+            return True
+            
+        else:
+        
+            return False
+
+# ======== End of Replay Buffer class ==================================================
+
 
 #components used
 self.ecg = self.controller.gModuleList['ecg']
@@ -14,3 +127,18 @@ self.stimOnLeft = 0
 self.onTimeRight = 0.0
 self.stimDurRight = 0.5
 self.stimOnRight = 0
+
+#number of trial
+self.numTrial = 0
+
+#make an asynchronous stimulus scheduling object
+self.asynch = ReplayBuffer()
+
+#percent duration of the original interbeat interval 
+self.asynchPercent = 100.0
+
+#prepare output logging
+now = datetime.today()
+self.csvLogger = csv.writer(open(path.join(self.controller.gLogger.Path, now.strftime('%y%m%d%H%M%S_') + self.initConf['name'] +  '.csv') , 'w'), lineterminator = '\n')
+hdr = ['time','trial','percentAsynch','beatSynch','beatAsynch','stimLeft','stimRight']
+self.csvLogger.writerow(hdr)
--- experiments/CardioVisualExp/script/updateLeftIsSync.py
+++ experiments/CardioVisualExp/script/updateLeftIsSync.py
@@ -1,19 +1,37 @@
+#current time
+currTime = time()
+
 #reset variables at first iteration
 if self.starting:
+    
+    #raise the trial number
+    self.numTrial+=1
+    
+    #both images to the back
+    self.imgLeft.depth = -1.5
+    self.imgRight.depth = -1.5
+    
     #stimulus Left runtime variables
     self.onTimeLeft = 0.0
     self.stimOnLeft = 0
+    
     #stimulus Right runtime variables
     self.onTimeRight = 0.0
     self.stimOnRight = 0
+    
+    #make new run for the asynch condition
+    self.asynch.newRun(currTime,self.asynchPercent)
 
-   
 #Stimulus Left = Sync
 if self.stimOnLeft == 0:
-    if self.ecg.realHeartbeat == 1:
-        self.onTimeLeft = time()
+    if self.ecg.realHeartbeat:
+        #record a new heartbeat
+        self.asynch.addBeat(currTime)
+        self.onTimeLeft = currTime
         self.stimOnLeft = 1
         self.imgLeft.depth = 1.5
+        line = [currTime,self.numTrial,self.asynchPercent,1,0,1,0]
+        self.csvLogger.writerow(line)     
 else:
     if time()-self.onTimeLeft > self.stimDurLeft:
         self.stimOnLeft = 0
@@ -22,12 +40,15 @@ else:
     
 #Stimulus Right = Async
 if self.stimOnRight == 0:
-    if self.ecg.beat == 1:
-        self.onTimeRight = time()
+    if self.asynch.getBeat(currTime):
+        #schedule next asynch beat
+        self.asynch.scheduleNextBeat(currTime)
+        self.onTimeRight = currTime
         self.stimOnRight = 1
         self.imgRight.depth = 1.5
+        line = [currTime,self.numTrial,self.asynchPercent,0,1,0,1]
+        self.csvLogger.writerow(line)  
 else:
     if time()-self.onTimeRight > self.stimDurRight:
         self.stimOnRight = 0
         self.imgRight.depth = -1.5
-    
